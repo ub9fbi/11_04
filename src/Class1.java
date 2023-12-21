84ac033cabfa6b2a6de949a276512b92f0a4cabc
ub9fbi <zolotoianatolji@yandex.com>
2023-12-21 12:04:56 +0500

    v1.0

---
+++ src/Class1.java
@@ -0,0 +1,24 @@
+public class Class1 {
+    public double x1,x2,y1,y2;
+
+    public Class1(double x1, double x2, double y1, double y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+    // Определение является ли прямоугольник квадратом
+    public boolean isSquare() {
+        double sideLength = Math.sqrt(Math.pow(x2 - x1, 2)) + Math.pow(y2 - y1, 2);
+        double diagonalLength = Math.sqrt(Math.pow(x1 - x2, 2)) + Math.pow(y1 - y2, 2);
+        if (sideLength == diagonalLength){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void isGraf () {
+
+    }
+}
---
+++ src/Class2.java
@@ -0,0 +1,6 @@
+public class Class2 extends Class1{
+
+    public Class2(double x1, double x2, double y1, double y2) {
+        super(x1, x2, y1, y2);
+    }
+}
