cfc96d9a99f8ed48dd499dd806d6627c8b5dcc7a
ub9fbi <zolotoianatolji@yandex.com>
2023-12-21 17:07:33 +0500

    v1.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,94 @@
-import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+
+class Rectangle {
+    int x1, y1, x2, y2;
+
+    public Rectangle(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public boolean isSquare() {
+        int width = Math.abs(x2 - x1);
+        int height = Math.abs(y2 - y1);
+        return width == height;
+    }
+
+    public void drawRectangle() {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Rectangle");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            BufferedImage image = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = image.createGraphics();
+
+            g2d.setColor(Color.GREEN);
+            int width = Math.abs(x2 - x1);
+            int height = Math.abs(y2 - y1);
+            g2d.fillRect(x1, y1, width, height);
+            g2d.setColor(Color.BLACK);
+            g2d.drawRect(x1, y1, width, height);
+            g2d.drawString("Rectangle #" + hashCode(), x1, y1 + height + 15);
+
+            ImageIcon icon = new ImageIcon(image);
+            JLabel label = new JLabel(icon);
+
+            frame.getContentPane().add(label);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+    }
+}
+
+class FilledRectangle extends Rectangle {
+    String text;
+    Color fillColor;
+
+    public FilledRectangle(int x1, int y1, int x2, int y2, String text, Color fillColor) {
+        super(x1, y1, x2, y2);
+        this.text = text;
+        this.fillColor = fillColor;
+    }
+
+    public int calculatePerimeterAndDiagonal() {
+        int width = Math.abs(x2 - x1);
+        int height = Math.abs(y2 - y1);
+        int perimeter = 2 * (width + height);
+        int diagonal = (int) Math.sqrt(width * width + height * height);
+        return perimeter + diagonal;
+    }
+
+    public void drawFilledRectangle() {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Filled Rectangle");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            BufferedImage image = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = image.createGraphics();
+
+            g2d.setColor(fillColor);
+            int width = Math.abs(x2 - x1);
+            int height = Math.abs(y2 - y1);
+            g2d.fillRect(x1, y1, width, height);
+            g2d.setColor(Color.BLACK);
+            g2d.drawRect(x1, y1, width, height);
+            g2d.drawString(text, x1, y1 + height + 15);
+
+            ImageIcon icon = new ImageIcon(image);
+            JLabel label = new JLabel(icon);
+
+            frame.getContentPane().add(label);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
@@ -29,7 +118,7 @@ public class Main {
             y2 = Integer.parseInt(y2Field.getText());
 
             Rectangle rectangle = new Rectangle(x1, y1, x2, y2);
-            FilledRectangle filledRectangle = new FilledRectangle(x1, y1, x2, y2, "Filled Rectangle", Color.YELLOW);
+            FilledRectangle filledRectangle = new FilledRectangle(x1, y1, x2, y2, "Filled Rectangle", Color.GREEN);
 
             rectangle.drawRectangle();
             filledRectangle.drawFilledRectangle();
