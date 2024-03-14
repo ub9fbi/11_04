34897887ea029c793866af4dd398bffd3c951e5b
ub9fbi <zolotoianatolji@yandex.com>
2024-03-14 22:30:37 +0500

    1

--- src/Time.java
+++ src/Time.java
@@ -5,13 +5,6 @@ import java.awt.image.BufferedImage;
 public class Time {
     int hour, min, sec;
 
-    public Time(int hour, int min, int sec) {
-        this.hour = hour;
-        this.min = min;
-        this.sec = sec;
-    }
-
-
     public int sumTimeSecond() {
         int sumTimeSecond = (hour * 3600) + (min * 30) + sec;
         return sumTimeSecond;
---
+++ src/Train.java
@@ -0,0 +1,16 @@
+import javax.xml.crypto.Data;
+
+public class Train extends Time {
+    public String trainNumber;
+    public String direction;
+    public Data departureTime;
+
+    public int minutesBeforeDeparture(String trainNumber, Data time){
+
+
+    }
+
+
+
+
+}
