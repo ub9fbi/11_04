47d5bbb4bac593fe361b21ecae08b58f2b42cba1
ub9fbi <zolotoianatolji@yandex.com>
2024-04-12 13:37:33 +0500

    V_1.0 добавлен обработчик событий для Spinner

--- src/Example_11_04.java
+++ src/Example_11_04.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class Example_11_04 extends JFrame {
@@ -9,11 +13,11 @@ public class Example_11_04 extends JFrame {
     //  поля ввода
     private JTextField jTextFieldTimeHour;
     private JTextField jTextFieldTimeMin;
-    private JTextField jTextFieldTimeSec;
-    private JTextField jTextFieldNumbTrain;
-    private JTextField jTextFieldDirectTrain;
-    private JTextField jTextFieldHourDepartTrain;
-    private JTextField jTextFieldMinDepartTrain;
+    private JFormattedTextField jFormattedTextTimeSec;
+    private JFormattedTextField jFormattedTextNumbTrain;
+    private JFormattedTextField jFormattedTextDirectTrain;
+    private JFormattedTextField jFormattedTextHourDepartTrain;
+    private JFormattedTextField jFormattedTextMinDepartTrain;
     private JTextField jTextFieldCount;
     private JButton jButtonNewTrain;
     private JButton jButtonDeleteTrain;
@@ -35,17 +39,20 @@ public class Example_11_04 extends JFrame {
     public Example_11_04() {
         initComponents();
         trainArrayList = new ArrayList<Train>();
-        currentInd=-1;
+        currentInd = trainArrayList.size() -1;
+
+
+
     }
 
     private void initComponents() {
         jTextFieldTimeHour = new javax.swing.JTextField();
         jTextFieldTimeMin = new javax.swing.JTextField();
-        jTextFieldTimeSec = new javax.swing.JTextField();
-        jTextFieldNumbTrain = new javax.swing.JTextField();
-        jTextFieldDirectTrain = new javax.swing.JTextField();
-        jTextFieldHourDepartTrain = new javax.swing.JTextField();
-        jTextFieldMinDepartTrain = new javax.swing.JTextField();
+        jFormattedTextTimeSec = new javax.swing.JFormattedTextField();
+        jFormattedTextNumbTrain = new javax.swing.JFormattedTextField();
+        jFormattedTextDirectTrain = new javax.swing.JFormattedTextField();
+        jFormattedTextHourDepartTrain = new javax.swing.JFormattedTextField();
+        jFormattedTextMinDepartTrain = new javax.swing.JFormattedTextField();
         jTextFieldCount = new javax.swing.JTextField();
         jButtonNewTrain = new javax.swing.JButton();
         jButtonDeleteTrain = new javax.swing.JButton();
@@ -62,8 +69,104 @@ public class Example_11_04 extends JFrame {
         jLabelDepartHour = new javax.swing.JLabel();
         jLabelDepartMin = new javax.swing.JLabel();
         jSpinnerCrn = new javax.swing.JSpinner();
-    }
+        jButtonNewTrain.addActionListener(new ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonNewTrainActionPerformed(evt);
+            }
+        });
 
+        jButtonDeleteTrain.addActionListener(new ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonDeleteTrainActoinPerformed(evt);
+            }
+        });
+        jButtonCalcTimeSeconds.addActionListener(new ActionListener() {
+
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonCalcTimeSeconds(evt);
+            }
+        });
+        jSpinnerCrn.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                jSpinnerCrnStateChanged(evt);
+            }
+
+        });
+    }
+    protected void getTrain() {
+        if (currentInd >= 0) {
+            jTextFieldTimeHour.setText(String.valueOf(trainArrayList.get(currentInd).hour));
+            jTextFieldTimeMin.setText(String.valueOf(trainArrayList.get(currentInd).min));
+            jFormattedTextTimeSec.setText("" + trainArrayList.get(currentInd).sec);
+            jFormattedTextNumbTrain.setText("" + trainArrayList.get(currentInd).trainNumber);
+            jFormattedTextDirectTrain.setText("" + trainArrayList.get(currentInd).directionTrain);
+            jFormattedTextHourDepartTrain.setText("" + trainArrayList.get(currentInd).departureHour);
+            jFormattedTextMinDepartTrain.setText("" + trainArrayList.get(currentInd).departureMin);
+        }
+    }
+    protected void setTrain() {
+        if ((currentInd >= 0) && ((jTextFieldTimeHour.getText() != null) && (jTextFieldTimeMin.getText() != null))) {
+            trainArrayList.get(currentInd).hour = Integer.parseInt(jTextFieldTimeHour.getText());
+            trainArrayList.get(currentInd).min = Integer.parseInt(jTextFieldTimeMin.getText());
+            try {
+                trainArrayList.get(currentInd).sec = new Integer(jFormattedTextTimeSec.getText());
+            } catch (Exception e) {
+                trainArrayList.get(currentInd).sec = 0;
+                jFormattedTextTimeSec.setText(null);
+            }
+            try {
+                trainArrayList.get(currentInd).trainNumber = new Integer(jFormattedTextNumbTrain.getText());
+            } catch (Exception e) {
+                trainArrayList.get(currentInd).trainNumber = 0;
+                jFormattedTextNumbTrain.setText(null);
+            }
+
+
+        }
+    }
+    //  обработчик для кнопки "Новое"
+    public void jButtonNewTrainActionPerformed(ActionEvent e) {
+        setTrain();
+        trainArrayList.add(new Train());
+        currentInd = trainArrayList.size()-1;
+        getTrain();
+        jTextFieldCount.setText("" + (currentInd + 1));
+        jSpinnerCrn.setValue(currentInd);
+    }
+    //  обработчик для кнопки удаления
+    private void jButtonDeleteTrainActoinPerformed (ActionEvent evt) {
+        trainArrayList.remove(currentInd);
+        currentInd = trainArrayList.size()-1;
+        if (currentInd == trainArrayList.size()) {
+            currentInd--;
+            jSpinnerCrn.setValue(currentInd);
+        } else {
+            getTrain();
+        }
+        jTextFieldCount.setText("" + trainArrayList.size());
+    }
+    // обработчик для кнопки расчета времени в секундах
+    private void jButtonCalcTimeSeconds (ActionEvent evt) {
+        setTrain();
+        for (int i = 0; i < trainArrayList.size(); i++) {
+            trainArrayList.get(i).sumTimeSecond();
+        }
+        getTrain();
+    }
+    public void jSpinnerCrnStateChanged(ChangeEvent e) {
+        setTrain();
+        currentInd = (int) jSpinnerCrn.getValue();
+        if (currentInd > trainArrayList.size() - 1) {
+            currentInd = trainArrayList.size() - 1;
+            jSpinnerCrn.setValue(currentInd);
+
+        } else if (currentInd < 0) {
+            currentInd = 0;
+            jSpinnerCrn.setValue(currentInd);
+        }
+        getTrain();
+
+    }
     public static void main(String[] args) {
         JFrame frame = new JFrame("Example_11_04");
         frame.setContentPane(new Example_11_04().JPanel);
